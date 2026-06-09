15f9c3f8e5a1744b4fa647f60dde03b52528adf3
eagudelowoden <eagudelo@woden.com.co>
2026-06-09 10:42:45 -0500

    Ajustes para desplegar

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "oms",
+      script: "npm",
+      args: "start",
+      cwd: "C:\\Users\\e.agudelo\\Documents\\OMS\\oms",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
